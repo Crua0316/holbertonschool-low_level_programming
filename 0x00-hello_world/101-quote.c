3ceae5ee00e367b64b21612cf26201583a2544bd
[Crua0316] <[cresrugi@gmail.com]>
2020-06-10 23:08:53 +0000

    Advence

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main - writes text to stdout
- * 
- * Return: returns 1
-*/
+ *main - writes text to stdout
+ *
+ *Return: returns 1
+ */
 int main(void)
 {
-	write("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
