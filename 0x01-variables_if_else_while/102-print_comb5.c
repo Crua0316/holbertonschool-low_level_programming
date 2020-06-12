8c6829655ad86fab485cf534931750bb1243167f
[Crua0316] <[cresrugi@gmail.com]>
2020-06-12 03:19:24 +0000

    advance 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <math.h>
-
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, i2;
+	int a, b;
 
-	i = 0;
-	while (i <= 98)
+	a = 0;
+	while (a <= 98)
 	{
-		i2 = i + 1;
-		while (i2 <= 99)
+		b = a + 1;
+		while (b <= 99)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
 			putchar(' ');
-			putchar(i2 / 10 + '0');
-			putchar(i2 % 10 + '0');
-			if (i == 98 && i2 == 99)
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+			if (a == 98 && b == 99)
 			{
 				putchar('\n');
 			}
@@ -30,9 +28,9 @@ int main(void)
 				putchar(',');
 				putchar(' ');
 			}
-			i2++;
+			b++;
 		}
-		i++;
+		a++;
 	}
 	return (0);
 }
