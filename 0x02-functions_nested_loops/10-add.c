f75c6050066d0a5515ee2686d0a68b2c26a4b20f
Crua0316 <cresrugi@gmail.com>
2020-06-16 20:24:55 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - Two integers and returns the result
+ *
+ * @a: first input
+ *
+ * @b: second input
+ *
+ * Return: last digit
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
