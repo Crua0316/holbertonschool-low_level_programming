d1d36eeca12d7220f5c0ffe05cd679481c79c9fb
Crua0316 <cresrugi@gmail.com>
2020-06-23 20:25:33 +0000

    Task 4 Rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_rev - prints a string in reverse
+ *@s: string
+ */
+
+void print_rev(char *s)
+{
+int a = 0;
+
+while (s[a] != '\0')
+{
+a++;
+}
+a = a - 1;
+while (a >= 0)
+{
+_putchar(s[a]);
+a--;
+}
+_putchar('\n');
+}
