3b2bd6653e76cbb18dcdea39ebd7c52b8d70fa40
Crua0316 <cresrugi@gmail.com>
2020-06-24 00:42:35 +0000

    Task 5-rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *rev_string - reverse a string
+ *@s: Var string
+ */
+
+void rev_string(char *s)
+{
+int a = 0;
+int b = 0;
+char H;
+
+while (s[a] != '\0')
+{
+a++;
+}
+a = a - 1;
+while (a > b)
+{
+H = s[b];
+s[b] = s[a];
+s[a] = H;
+a--;
+b++;
+}
+}
