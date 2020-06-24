34f2496825de4f92a6593b72f9f92b3962dd404f
Crua0316 <cresrugi@gmail.com>
2020-06-24 16:36:34 +0000

    Fixed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,24 @@
 #include "holberton.h"
 
 /**
- *_strcpy - copies string
- *@dest: string value
- *@src: Pointer
- *Return: Dest
+ * _strcpy - copies string
+ * @dest: string value
+ * @src: Pointer
+ * Return: Dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
-int a = 0;
-int b = 0;
+	int a = 0;
+	int b = 0;
 
-while (src[a] != '\0')
-{
-a++;
-}
-for (; b < a; b++)
-{
-dest[b] = src[b];
-}
-return(dest);
+	while (src[a] != '\0')
+	{
+		a++;
+	}
+	for (; b < a; b++)
+	{
+		dest[b] = src[b];
+	}
+	return (dest);
 }
