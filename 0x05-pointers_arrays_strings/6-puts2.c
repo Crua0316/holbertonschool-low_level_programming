de0d575c7b24d89e4ca3f9552b3f410c2cfd752b
Crua0316 <cresrugi@gmail.com>
2020-06-24 01:01:47 +0000

    Fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,14 @@
 
 void puts2(char *str)
 {
-int a;
+	int a;
 
-a = 0;
-while (str[a] != '\0')
-{
-if (a % 2 == 0 && a >= 0)
-_putchar(str[a]);
-a++;
-}
-_putchar(10);
+	a = 0;
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0 && a >= 0)
+			_putchar(str[a]);
+		a++;
+	}
+	_putchar(10);
 }
