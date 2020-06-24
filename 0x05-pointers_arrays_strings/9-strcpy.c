21ae839e76eed0b7274bb82e4647920206053cf7
Crua0316 <cresrugi@gmail.com>
2020-06-24 16:42:49 +0000

    Fixed 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int a = 0;
-	int b = 0;
+	int a = 0, b;
 
 	while (src[a] != '\0')
-	{
 		a++;
-	}
-	for (; b < a; b++)
+	for (b = 0; b <= a; b++)
 	{
 		dest[b] = src[b];
 	}
