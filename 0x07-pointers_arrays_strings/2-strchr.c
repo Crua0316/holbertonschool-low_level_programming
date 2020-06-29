70feb5cf073d1fb172c4ca44fcfebc2f79946812
Crua0316 <cresrugi@gmail.com>
2020-06-29 17:57:51 +0000

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strchr - Fills memory with a constant byte
+ * @s: Pointer
+ * @c: Variable char
+ * Return: dest
+ */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+	char *temp;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == c)
+		{
+			temp = &s[a];
+			return (temp);
+		}
+	}
+	return ('\0');
+}
