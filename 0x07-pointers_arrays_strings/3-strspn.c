1f87f57ef851ab9e532431504c9ddbb2c12b68d5
Crua0316 <cresrugi@gmail.com>
2020-06-29 19:29:05 +0000

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strspn - Fills memory with a constant byte
+ * @s: Pointer
+ * @accept: Variable char
+ * Return: String
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int str;
+	int a, b;
+
+	str = 0;
+	for (a = 0; s[a] != ' '; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				str = str + 1;
+			}
+		}
+	}
+	return (str);
+}
