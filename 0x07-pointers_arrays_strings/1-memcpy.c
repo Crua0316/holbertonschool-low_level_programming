1bb091c227c7f621243aa0c0de5877d382e42e0f
Crua0316 <cresrugi@gmail.com>
2020-06-29 17:22:08 +0000

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memset - Fills memory with a constant byte
+ * @dest: Pointer
+ * @src: Variable char
+ * @n: unsigned var
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
