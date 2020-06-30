f51af959f032fb88ca9e5dc4115ff747d7d4dc97
Crua0316 <cresrugi@gmail.com>
2020-06-30 13:59:02 +0000

    Task 8

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - set the value of a pointer
+ * @s: double pointer
+ * @to: pointer
+ * Return: no return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
