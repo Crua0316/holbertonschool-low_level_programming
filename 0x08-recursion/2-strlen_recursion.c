f5d4aa8327718a354f62f3c9f0e5c45cd0610edd
Crua0316 <cresrugi@gmail.com>
2020-07-01 18:53:24 +0000

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - Print a string in reverse
+ * @s: Pointer to string
+ * Return: void
+ */
+
+int _strlen_recursion(char *s)
+{
+    int l = 0;
+
+	if (*s == 0)
+    {
+        return (0);
+    }
+    else
+    {
+        l = (_strlen_recursion (s + 1));
+        return(l + 1);
+    }
+    
+	
+}
