86955fffd6e51034e4b02082155eeb868520239f
Crua0316 <cresrugi@gmail.com>
2020-07-01 23:31:50 +0000

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+/**
+ *_sqrt_recursion - blank
+ * @n: blank
+ * Return: balnk
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+    {
+		return (0);
+    }
+	else if (n == 1)
+    {
+		return (1);
+    }
+	else if (n < 0)
+    {
+		return (-1);
+    }
+	else
+    {
+		return (recursion(n, 1));
+    }
+}
+
+/**
+ * recursion - recursion root
+ * @a: number
+ * @b: incrementor
+ * Return: -1;
+ */
+
+int recursion(int a, int b)
+{
+	if (a == (b * b))
+    {
+		return (b);
+    }
+	else if (a > (b * b))
+    {
+		return (recursion(a, b + 1));
+    }
+	else
+    {
+		return (-1);
+    }
+}
