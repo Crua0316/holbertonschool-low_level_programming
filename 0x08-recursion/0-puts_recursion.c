350adb2a8171bec1fe09f35f8d0ec95b95bf3d77
Crua0316 <cresrugi@gmail.com>
2020-07-01 08:18:10 +0000

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - Prints a string
+ * @s: Pointer to string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar (10);
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
