a145ff6e6ef09536117de578b512b57f9ac08b50
Crua0316 <Crua0316>
2020-07-15 09:22:11 +0000

    Task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+* print_name - function that prints a name.
+* @name: Variable of name
+* @f: pointer
+*
+* Return: 0
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == '\0')
+		return;
+
+	(*f)(name);
+}
