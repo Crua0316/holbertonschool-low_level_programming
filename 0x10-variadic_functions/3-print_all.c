1ac6b07ac43af3bda60bd696bf9d0576d0b45201
Crua0316 <Crua0316>
2020-07-18 16:50:08 +0000

    Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+#include "type_of_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed
+ *
+ * Return: list
+ */
+
+void print_all(const char * const format, ...)
+{
+	fts formats[] = {
+		{"c", f_char},
+		{"i", f_int},
+		{"f", f_float},
+		{"s", f_str},
+		{NULL, NULL},
+	};
+
+	int a, b;
+	va_list list;
+	char *s = "";
+
+	a = 0;
+	va_start(list, format);
+
+	while (format && format[a])
+	{
+		b = 0;
+		while (formats[b].s_fts != NULL)
+		{
+			if (formats[b].s_fts[0] == format[a])
+			{
+				printf("%s", s);
+				formats[b].func(list);
+				s = ", ";
+			}
+			b++;
+		}
+		a++;
+	}
+	printf("\n");
+	va_end(list);
+}
