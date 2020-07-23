b2753cd35baf7964b106d9f8c88c68d921ec611f
Crua0316 <cresrugi@gmail.com>
2020-07-23 23:46:50 +0000

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t
+ * @h: pointer
+ * 
+ * Return: Nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+        size_t count = 0;
+
+        if (h == NULL)
+        {
+               return (0); 
+        }
+
+        while (h != NULL)
+        {
+                if (h->str)
+                        printf("[%u] %s\n", h->len, h->str);
+                else
+                        printf("[0] (nil)\n");
+                        h = h->next;
+                        count++;
+                
+        }
+        return (count);
+}
