f99afba1674b57a5592ef9478bb03fc19c95940d
Crua0316 <cresrugi@gmail.com>
2020-07-24 02:35:54 +0000

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: pointer to linked
+ * 
+ * Return: void return
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
