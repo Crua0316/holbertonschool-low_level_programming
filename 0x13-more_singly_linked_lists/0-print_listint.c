9cfb6cfe3f6b2da54c9924e000eb796402d9a094
Crua0316 <cresrugi@gmail.com>
2020-07-28 20:49:41 +0000

    Fix trailing whitespace

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,13 +3,13 @@
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: Pointer
- * 
+ *
  * Return: The number of nodes
  */
 
 size_t print_listint(const listint_t *h)
 {
-	
+
 	size_t count = 1;
 
 	if (h == NULL)
