5c71f614da02be69aa92ec5485bc66cdc650d26d
Crua0316 <cresrugi@gmail.com>
2020-07-29 01:03:02 +0000

    Fixed betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a listint_t list
- * @head: pointer
- *
- * Return: Node to end
+ * free_listint2 - frees a listint_t list
+ * @head: double pointer
+ * Return: Null
  */
 
 void free_listint2(listint_t **head)
 {
 	listint_t *aux;
 
-	if(!head)
+	if (!head)
 		return;
 	while (*head != NULL)
 	{
