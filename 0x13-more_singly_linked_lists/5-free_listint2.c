0a3d1441065119b1b8c094d5105a615b066e4111
Crua0316 <cresrugi@gmail.com>
2020-07-29 01:00:24 +0000

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer
+ *
+ * Return: Node to end
+ */
+
+void free_listint2(listint_t **head)
+{
+        listint_t *aux;
+
+        if(!head)
+                return;
+        while (*head != NULL)
+        {
+                aux = (*head)->next;
+                free(*head);
+                *head = aux;
+        }
+        head = NULL;
+}
