b60c17a36879f7339f03a25c3dabf9dfd57cd7d1
Crua0316 <cresrugi@gmail.com>
2020-07-29 01:01:30 +0000

    Fix betty

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,15 +9,15 @@
 
 void free_listint2(listint_t **head)
 {
-        listint_t *aux;
+	listint_t *aux;
 
-        if(!head)
-                return;
-        while (*head != NULL)
-        {
-                aux = (*head)->next;
-                free(*head);
-                *head = aux;
-        }
-        head = NULL;
+	if(!head)
+		return;
+	while (*head != NULL)
+	{
+		aux = (*head)->next;
+		free(*head);
+		*head = aux;
+	}
+	head = NULL;
 }
