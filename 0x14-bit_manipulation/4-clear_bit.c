eb54756e7416ea0f0e9426673273c5fcc071bb2f
Crua0316 <cresrugi@gmail.com>
2020-08-01 11:17:51 +0000

    Task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * set_bit - returns the value of a bit at a given index
+ * @n: Value decimal
+ * @index:  is the index, starting from 0 of the bit you want to get
+ *
+ * Return: 
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int a;
+
+	if (index > sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	a = ~(1 << index);
+	*n = *n & a;
+	return (1);
+}
