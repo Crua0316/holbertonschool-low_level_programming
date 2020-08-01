aa5b5a9f8f4c961eb74b1829b3f3a254ce87dbb9
Crua0316 <cresrugi@gmail.com>
2020-08-01 11:21:15 +0000

    Fix Betty

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * set_bit - returns the value of a bit at a given index
+ * clear_bit - sets the value of a bit to 0 at a given index.
  * @n: Value decimal
- * @index:  is the index, starting from 0 of the bit you want to get
+ * @index:  is the index, starting from 0 of the bit you want to set
  *
- * Return: 
+ * Return: 1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
