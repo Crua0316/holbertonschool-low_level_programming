7e551e2d9e13bc3ddd618f4b7b2aac15279b79ae
Crua0316 <cresrugi@gmail.com>
2020-08-05 04:04:45 +0000

    Fix Task number 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX
+ * read_textfile - reads a text file and prints it to the POSIX.
  * @filename: Name file
  * @letters: Letters
  *
@@ -15,9 +15,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	    write_e = 0;
 	char *buff;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-
+		
 	f_d = open(filename, O_RDONLY);
 	if (f_d == -1)
 		return (0);
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	read_d = read(f_d, buff, letters);
 	write_e = write(STDOUT_FILENO, buff, letters);
-	if (!write_e)
+	if (write_e == -1)
 		return (0);
 
 	free(buff);
