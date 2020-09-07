6d12f565d70dfa70f57cf413dbc0c4b13b5ae466
Crua0316 <cresrugi@gmail.com>
2020-09-07 02:58:36 +0000

    Add librerias

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef LIST_H
 #define LIST_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -17,7 +20,7 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
-Prototype: size_t print_dlistint(const dlistint_t *h);
+
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
